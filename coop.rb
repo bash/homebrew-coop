5efc25d42f03fa73286c2fb4d3d273c4d526cc24
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-04-30 16:16:30 +0200

    new release

--- coop.rb
+++ coop.rb
@@ -2,9 +2,9 @@ class Coop < Formula
   desc "The official homebrew formula for STJEREM/coop"
   homepage "https://github.com/STJEREM/coop"
   head "https://github.com/STJEREM/coop.git", :branch => 'development'
-  url 'https://github.com/STJEREM/coop/archive/v0.0.1.zip'
-  sha256 'aded762d2883267144d3c78124483caffd153d302969a5100a27b3e6fe1f935e'
-  version '0.0.1'
+  url 'https://github.com/STJEREM/coop/archive/0.0.3.zip'
+  sha256 '1ffa2b1155e6f1ad03d48e3a8ea72b2cac477efaa76a701104717bca2d965436'
+  version '0.0.3'
 
   bottle :unneeded
 
