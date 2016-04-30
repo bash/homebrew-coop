68bf18e84f41f4f54f346b98c9273710879dbc06
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-04-30 13:17:48 +0200

    add zip url

--- coop.rb
+++ coop.rb
@@ -2,7 +2,11 @@ class Coop < Formula
   desc "The official homebrew formula for STJEREM/coop"
   homepage "https://github.com/STJEREM/coop"
   head "https://github.com/STJEREM/coop.git", :branch => 'development'
-  
+  url 'https://github.com/stjerem/coop/archive/52009ae98d16a690d2ce644364d9cbd81bc9d463.zip'
+  sha256 'e59d5c4c48c9a6e95afdf395d54652bdcb674cd6a4cfe5ce374c2a328b0c3b76'
+
+  bottle :unneeded
+
   depends_on 'python3'
 
   def install
