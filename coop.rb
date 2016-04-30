d420f51a3c883fddc712ad30237c7f4cefc2d65c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-04-30 11:30:28 +0200

    add coop.rb

---
+++ coop.rb
@@ -0,0 +1,12 @@
+class Coop < Formula
+  desc "The official brew formula for STJEREM/coop"
+  homepage "https://github.com/STJEREM/coop"
+  head "https://github.com/STJEREM/coop.git", :branch => 'development'
+  
+  depends_on 'python3'
+
+  def install
+    system "python3", "-m", "pip", "install", "-r", "client/requirements.txt"
+    bin.install "client/coop"
+  end
+end
