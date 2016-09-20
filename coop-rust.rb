5fc06809b60950b637880f73c27c5768ae8019a9
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-09-20 19:03:17 +0200

    Add conflict

--- coop-rust.rb
+++ coop-rust.rb
@@ -5,6 +5,7 @@ class CoopRust < Formula
   url 'https://github.com/bash/coop-rust/releases/download/v0.0.1/coop-x86_64-apple-darwin.zip'
   sha256 'eb5df11bc85671657e2caa8ec501d4d29893640020ee012f7fb0d072e8adcfa2'
   version '0.0.1'
+  conflicts_with 'coop'
 
   bottle :unneeded
 
