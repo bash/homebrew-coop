1672ddb044bd2e5b3c3e074ce3f47a5319c9e889
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-09-20 23:00:05 +0200

    Update checksum

--- coop-rust.rb
+++ coop-rust.rb
@@ -3,7 +3,7 @@ class CoopRust < Formula
   homepage 'https://github.com/bash/coop-rust'
   head 'https://github.com/bash/coop-rust.git', :branch => 'development'
   url 'https://github.com/bash/coop-rust/releases/download/v0.0.2/coop-x86_64-apple-darwin.zip'
-  sha256 'f45d10dbb37c6daeaefecfb16f3319af1923d5cfdaac0c953e1457fd497cb115'
+  sha256 'b2c52772c52429ffcfe73e5177e2b04db2a659242841a29e30379785c4dfd1ea'
   version '0.0.2'
   conflicts_with 'coop'
 
