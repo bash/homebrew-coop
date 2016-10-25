58702f784745b1d95893ee97de1401611e04828f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-10-25 15:24:59 +0200

    Update coop-rust version

--- coop-rust.rb
+++ coop-rust.rb
@@ -2,9 +2,9 @@ class CoopRust < Formula
   desc 'Homebrew formula for coop-rust'
   homepage 'https://github.com/bash/coop-rust'
   head 'https://github.com/bash/coop-rust.git', :branch => 'development'
-  url 'https://github.com/bash/coop-rust/releases/download/v0.1.0/coop-x86_64-apple-darwin.zip'
-  sha256 '96b9ae53f474e12878b4ecfc510f0927eb6b933e7af13321df1d748cf6b6d022'
-  version '0.1.0'
+  url 'https://github.com/bash/coop-rust/releases/download/v0.1.1/coop-x86_64-apple-darwin.zip'
+  sha256 '03db7824b13c6fd47cc0ea4a8b3f7d3f37610c7f0769267106a0468fe9537270'
+  version '0.1.1'
   conflicts_with 'coop'
 
   bottle :unneeded
